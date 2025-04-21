442d5c47b1231f9d98a56383a93588aa082606dc
Ivangabriel21210 <ivangabriel21210@gmail.com>
2025-04-21 15:37:30 -0400

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,20 @@
+addEventListener("fetch", event => {
+  event.respondWith(
+    handleRequest(event.request)
+  );
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+
+  // Cambia esto por la IP o dominio de tu VPS
+  url.hostname = "holisprite.es"
+  url.protocol = "https:"
+  url.port = "443"
+
+  return fetch(url.toString(), {
+    method: request.method,
+    headers: request.headers,
+    body: request.body
+  })
+}
